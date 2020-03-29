eb58bde6891561c60a0df6b73592f6be1b55e36c
Jhubkonto <julian.lacis@gmail.com>
2020-03-29 22:50:45 +0200

    Added dynamic JavaScript behaviour

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var randomNumber1 = (Math.floor((Math.random() * 6) + 1));
+var randomNumber2 = (Math.floor((Math.random() * 6) + 1));
+
+// document.querySelector("img.img1").getAttributeNode("src").value =
+// "images/dice" + randomNumber1 + ".png";
+
+document.querySelector("img.img1").setAttribute("src", ("images/dice" + randomNumber1 + ".png"));
+
+document.querySelector("img.img2").setAttribute("src", ("images/dice" + randomNumber2 + ".png"));
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 P1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "P2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").textContent = "Draw!";
+}
